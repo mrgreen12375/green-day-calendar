7df31311d8a0d5d49082ebe47a0c7ad909596e10
Steven Green <ste2144105@gmail.com>
2022-09-01 20:08:20 -0700

    added comments for js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
+// setup the current date below the title.
 var todaysDate = moment().format("dddd, MMMM Do");
 $("#currentDay").text(todaysDate);
-
+// created variable for where the message will go in each text area.
 var message = Array(9).fill('');
-
+//created function for previous stored messages.
 function savedMessage() {
     var storedMessage = JSON.parse(localStorage.getItem("message"));
     if (storedMessage !== null) {
         message = storedMessage;
     }
 }
-
+//created function to save message. 
+//used event.target and traversed the dom to the text area. 
 function pushMessage(event) {
     var row = $(event.target).parent().parent();
     message[Number(row.attr('id'))] = row.children('textarea').val(); 
     localStorage.setItem("message", JSON.stringify(message));
 }
-
+// logs saved message.
 savedMessage();
-
+//created a for loop for the sections.
+//setting the time for each hour.
+//setting the color of the message field based of the time of day.
+//create the time, text, and save icon for every hour.
 for (var i = 0; i <= 8; i++) {
 
     var hour = moment(i + 9, 'h').format("ha");
@@ -40,5 +45,5 @@ for (var i = 0; i <= 8; i++) {
                                 </div>
                             </div>`);
 }
-
+// created listener to have the floppy disk icon save message.
 $('i').click(pushMessage);
